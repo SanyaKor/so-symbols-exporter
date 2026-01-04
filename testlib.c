3fef2f1f6d7f14c5982ad9a0b80b94ea147769a5
Aleksandr Lebedev <alexandr.2nd@gmail.com>
2026-01-04 11:36:46 +0100

    added test code

---
+++ testlib.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+#if defined(__GNUC__) || defined(__clang__)
+  #define VISIBILITY_DEFAULT __attribute__((visibility("default")))
+  #define VISIBILITY_HIDDEN __attribute__((visibility("hidden")))
+  #define VISIBILITY_WEAK   __attribute__((weak))
+#else
+  #define VISIBILITY_DEFAULT
+  #define VISIBILITY_HIDDEN
+  #define VISIBILITY_WEAK
+#endif
+
+
+/////////////////////////////////////
+
+int func1(int a, int b) {
+    return 0;
+}
+
+void func1(const char *msg) {
+}
+
+/////////////////////////////////////
+
+static int static_mul(int a, int b) {
+    return 0;
+}
+
+static void static_trace(const char *msg) {
+}
+
+/////////////////////////////////////
+
+VISIBILITY_DEFAULT int default_func1(int a, int b) {
+    return 0;
+}
+
+VISIBILITY_DEFAULT void default_func1(const char *msg) {
+}
+
+/////////////////////////////////////
+
+VISIBILITY_HIDDEN int hidden_func1(int a, int b) {
+    return 0;
+}
+
+VISIBILITY_HIDDEN void hidden_func2(const char *msg) {
+}
+
+
+/////////////////////////////////////
+
+VISIBILITY_WEAK int weak_func1(int a, int b) {
+    return 0;
+}
+
+VISIBILITY_WEAK void weak_func2(const char *msg) {
+}
+
+
+
