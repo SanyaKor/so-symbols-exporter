4db9cb2f8dc172fb55828d3387e6d9b66fd9ef50
Aleksandr Lebedev <alexandr.2nd@gmail.com>
2026-01-04 16:04:32 +0100

    refactoring: added help msgs for friendly UI

--- so_symbols_exporter.py
+++ so_symbols_exporter.py
@@ -1,4 +1,30 @@
 import subprocess
+import argparse
+import sys
+
+
+def print_table(symbols: list[dict[str, str]]) -> None:
+
+    if not symbols:
+        print("(no exported functions)")
+        return
+
+    name_w = max(len(s["name"]) for s in symbols)
+    name_w = max(name_w, 20)
+
+    header = f"{'NAME'.ljust(name_w)}  TYPE   BIND   VIS"
+
+    print(header)
+    print("-" * len(header))
+
+    for s in symbols:
+        print(
+            f"{s['name'].ljust(name_w)}  "
+            f"{s['type'].ljust(6)} "
+            f"{s['bind'].ljust(6)} "
+            f"{s['vis']}"
+        )
+
 
 def parse_readelf_symbol_line(line: str) -> dict[str, str] | None:
     parts = line.split()
@@ -24,7 +50,7 @@ def parse_readelf_symbol_line(line: str) -> dict[str, str] | None:
 # But personally i would prefer readelf, since there is much information about files, and structure of output is stable(Fixed table)
 def so_exported_functions(so_path: str, demangle : bool = True) -> list[dict[str, str]] | None:
 
-    cmd = ["readelf", "-Ws", "--syms"]
+    cmd = ["readelf", "-Ws"]
     if demangle:
         cmd.append("--demangle")
 
@@ -67,3 +93,41 @@ def so_exported_functions(so_path: str, demangle : bool = True) -> list[dict[str
 
     exported_symbols.sort(key=lambda x: x["name"])
     return exported_symbols
+
+def run():
+    parser = argparse.ArgumentParser(prog="so-symbols-exporter",description=(
+        "List native functions defined in a Linux shared object (.so).\n"
+        "Uses readelf to extract function symbols implemented in the file "
+        "(excluding imported/undefined symbols).\n\n"
+        "Example:\n"
+        "  so-symbols-exporter --file libexample.so\n"
+        "  so-symbols-exporter --file libexample.so --pretty-output"
+    ))
+    parser.add_argument("--file", required=True, help="Path to the .so file")
+
+    parser.add_argument(
+        "--pretty-output",
+        action="store_true",
+        help="Pretty-print output"
+    )
+    args = parser.parse_args()
+
+    try:
+        ex_symbols = so_exported_functions(args.file)
+        if args.pretty_output:
+            print_table(ex_symbols)
+        else:
+            for f in ex_symbols:
+                print(f["name"])
+
+    except subprocess.CalledProcessError as e:
+        err = (e.stderr or e.stdout or "").strip()
+        print(f"ERROR: readelf failed\n{err}", file=sys.stderr)
+        exit()
+    except Exception as e:
+        print(f"ERROR: {e}", file=sys.stderr)
+        exit()
+
+
+if __name__ == "__main__":
+    run()
