7e4b7aa4f740f8989ad99f31b946c0bbadeb98e9
Aleksandr Lebedev <alexandr.2nd@gmail.com>
2026-01-04 11:41:32 +0100

    added tool to analyze so files

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
-import requests
+import subprocess
+import argparse
 
-def main() -> None:
-    response = requests.get("https://httpbin.org/get", timeout=5)
-    print("Status:", response.status_code)
-    print("Origin:", response.json()["origin"])
+def parse_readelf_symbol_line(line: str) -> dict[str, str] | None:
+    parts = line.split()
+    if len(parts) < 8:
+        return None
+
+    keys = ["num", "value", "size", "type", "bind", "vis", "ndx", "name"]
+    return dict(zip(keys, parts[:8]))
+
+def so_exported_functions(so_path: str) -> set[str]:
+    p = subprocess.run(
+        ["readelf", "-Ws", so_path],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+
+    exported_funcs: set[str] = set()
+
+    for line in p.stdout.splitlines():
+        parsed_data = parse_readelf_symbol_line(line)
+        if not parsed_data:
+            continue
+
+
+        if (
+            parsed_data["type"] in ("FUNC", "IFUNC")
+            and parsed_data["ndx"] != "UND"
+            and parsed_data["bind"] in ("GLOBAL", "WEAK")
+        ):
+            exported_funcs.add(parsed_data["name"])
+
+    return exported_funcs
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(prog="sotest",
+                                     description="so?")
+    parser.add_argument("--file", required=True, help="file")
+
+    args = parser.parse_args()
+    exported_funcs = so_exported_functions(args.file)
